44f4e956f285d4742a7b027ab9aea62b1d640449
Longhaidang <153490754+Longhaidang@users.noreply.github.com>
2024-02-14 09:35:23 +0100

    Add files via upload

---
+++ haisJs.js
@@ -0,0 +1,12 @@
+function showModal() {
+    let modalbox = document.getElementById("modalbox");
+    modalbox.style.display = "block";
+}
+
+function closeModal() {
+    let modalbox = document.getElementById("modalbox");
+    modalbox.style.display = "none";
+}
+
+
+    
